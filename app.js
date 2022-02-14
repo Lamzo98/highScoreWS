dda191dd7780b39d0780f710c3e9833374eeabe4
Mamadou <lamaranabah58@gmail.com>
2022-02-14 13:50:19 -0400

    Mamadou

--- app.js
+++ app.js
@@ -1,23 +1,5 @@
 const express = require('express');
-//const db = require('./DBConnection');
-const mysql = require('mysql');
-
-let db = mysql.createConnection({
-    host     : process.env.RDS_HOSTNAME,
-    user     : process.env.RDS_USERNAME,
-    password : process.env.RDS_PASSWORD,
-    port     : process.env.RDS_PORT,
-    database : process.env.RDS_DB_NAME
-});
-
-db.connect((err) => {
-    if (err) {
-        console.error('Database connection failed: ' + err.stack);
-        return;
-    } 
-    console.log("Connected to MySQL...");
-});
-
+const db = require('./DBConnection');
 const Joi = require('joi');
 
 //Entry point
